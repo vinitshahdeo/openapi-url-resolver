d6b2bdca896b68f176d796d7e4eff10e394a17ce
Vinit Shahdeo <vinit.shahdeo@postman.com>
2023-04-20 01:49:44 +0530

    Added first-cut of openapi-url-resolver

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# openapi-url-resolver
-Resolve server URLs and extracts hosts from OpenAPI 3.0 and Swagger specifications.
+# OpenAPI URL Resolver
+
+`openapi-url-resolver` is an NPM package that provides a simple and efficient way to resolve URLs from OpenAPI specifications. It also removes protocols from the resolved URLs and allows you to easily extract host information from OpenAPI definitions. This package is ideal for developers working with APIs that conform to the OpenAPI specification and need to extract server information to make API calls.
+
+## Installation
+
+You can install `openapi-url-resolver` via NPM:
+
+```sh
+npm install openapi-url-resolver
+```
+
+## Usage
+
+It supports two methods:
+
+- `getServerUrls` - Return the server URLs from the OpenAPI 3.x specification.
+- `extractHosts` - Get hosts information from OpenAPI & Swagger definitions.
+
+```javascript
+const openapiUrlResolver = require('openapi-url-resolver');
+
+openapiUrlResolver.resolve(spec, false);
+const serverUrls = openapiUrlResolver.getServerUrls(spec, false);
+
+```
+
+
+To use `openapi-url-resolver`, you need to pass an OpenAPI specification object to the resolveServerUrls() function. This function will return an array of resolved server URLs:
+
+```javascript
+const openapiUrlResolver = require('openapi-url-resolver');
+
+const spec = {
+  openapi: '3.0.0',
+  servers: [
+    {
+      url: 'https://api.example.com/v1',
+      description: 'Production server'
+    },
+    {
+      url: 'https://staging-api.example.com/v1',
+      description: 'Staging server'
+    }
+  ]
+};
+
+const serverUrls = openapiUrlResolver.getServerUrls(spec);
+
+console.log(serverUrls); // ['api.example.com/v1', 'staging-api.example.com/v1']
+```
+
+To extract host information from an OpenAPI specification, you can use the getHosts() function:
+
+```javascript
+const openapiUrlResolver = require('openapi-url-resolver');
+
+const spec = {
+  openapi: '3.0.0',
+  servers: [
+    {
+      url: 'https://api.example.com/v1',
+      description: 'Production server'
+    },
+    {
+      url: 'https://staging-api.example.com/v1',
+      description: 'Staging server'
+    }
+  ]
+};
+
+const hosts = openapiUrlResolver.getHosts(spec);
+
+console.log(hosts); // ['api.example.com', 'staging-api.example.com']
+
+```
+
+## Contributing
+
+Contributions to openapi-url-resolver are welcome! If you find a bug or want to suggest a new feature, please open an issue on the GitHub repository. If you want to contribute code, please fork the repository, make your changes, and submit a pull request.
+
+## License
+
+`openapi-url-resolver` is released under the MIT License.
---
+++ src/index.js
@@ -0,0 +1,97 @@
+/**
+ * Returns an array of hosts from the Open API Definition provided in JSON format.
+ *
+ * @param {Object} spec - Open API Definition in JSON format
+ * @returns {Array} - Array containing host
+ */
+function getHosts(spec = {}) {
+    /**
+     * OpenAPI 2 (aka Swagger) contains host, the schemes like HTTP, HTTPS are present as a separate field.
+     * Learn more: https://swagger.io/specification/v2/
+     */
+    if (spec.version === '2.0') {
+        return spec.host;
+    }
+
+    // Get the server URLs
+    const serverUrls = getServerUrls(spec);
+
+    // Remove protocols (e.g. http://, https://) to get the host
+    const hosts = [];
+
+    for (const url of serverUrls) {
+        const host = url.replace(/(^\w+:|^)\/\//, '');
+        if (!hosts.includes(host)) {
+            hosts.push(host);
+        }
+    }
+
+    return hosts;
+}
+
+/**
+ * Resolves URLs for the OpenAPI spec's servers field.
+ * Learn about the server object: https://swagger.io/specification/
+ *
+ * @param {object} spec - The OpenAPI specification object.
+ * @param {object|array} spec.servers - The server object.
+ * @returns {string[]} An array of resolved server URLs.
+ */
+function getServerUrls(spec) {
+    const servers = Array.isArray(spec.servers) ? spec.servers : [spec.servers];
+
+    // Loop through each server object and resolve its URL
+    const serverUrls = [];
+    for (const server of servers) {
+        const { url, variables } = server;
+
+        if (!url) {
+            console.info('No URL found in server object:', server);
+            continue;
+        }
+
+        // Replace the variables in the URL with their values
+        let resolvedUrls = [url];
+
+        if (variables) {
+            for (const [varName, varValue] of Object.entries(variables)) {
+                let { default: defaultValue, enum: enumValues } = varValue;
+
+                if (!defaultValue && !enumValues) {
+                    console.info(`No default value or enumeration found for variable ${varName} in server object:`, server);
+                    continue;
+                }
+
+                if (!enumValues && defaultValue) {
+                    enumValues = [defaultValue];
+                } else if (!enumValues) {
+                    console.info(`No enumeration found for variable ${varName} in server object:`, server);
+                    continue;
+                }
+
+                // Replace the variable in the URL with each of its enumeration values
+                const valuesToReplace = [];
+
+                for (const value of enumValues) {
+                    for (const resolvedUrl of resolvedUrls) {
+                        const newUrl = resolvedUrl.replace(`{${varName}}`, value);
+                        if (!valuesToReplace.includes(newUrl)) {
+                            valuesToReplace.push(newUrl);
+                        }
+                    }
+                }
+
+                resolvedUrls = valuesToReplace;
+            }
+        }
+
+        serverUrls.push(...resolvedUrls);
+    }
+
+    return serverUrls;
+}
+
+module.exports = {
+    getServerUrls,
+    getHosts
+};
